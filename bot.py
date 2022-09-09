e5ab9e776a16e352badea15ebe7236e6ff2b45e5
Emily Rhee <emilyrhee99@gmail.com>
2022-09-09 15:54:49 -0400

    fix: token

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 import discord
 import random
 from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -20,5 +24,5 @@ async def on_message(message):
         wyd_responses = ["On the toilet.", "Wrapping up some work.", "Watching some YouTube."]
         await message.channel.send(random.choice(wyd_responses) + " Wbu?")
 
-bot.run('MTAxMzk2MTcxMTM2MDkzODAwNQ.GCnm2t.nFCx9vQDi5YRUcnDFKGp-0kVkCvBpAtd3OIBy0')
+bot.run(os.getenv('TOKEN'))
 
